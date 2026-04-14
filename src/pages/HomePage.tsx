531b3714fbb843d61d6c7033717202a257e12f71
Math2okt <matias.marecos@fiuni.edu.py>
2026-04-14 12:17:30 -0300

    feat: Add error dialog, error screen and loading screen components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,21 @@
-import { useMe, useLogout } from "@/queries/auth.queries";
+import { ErrorDialog } from "@/components/ui/screens/error-dialog";
+import { LoadingScreen } from "@/components/ui/screens/loading-screen";
+import { useMe } from "@/queries/auth.queries";
 import { useAuthStore }     from "@/stores/auth.store";
-import { Heading, Box} from "@chakra-ui/react";
+import { Heading, Box,Button} from "@chakra-ui/react";
 import { Link }             from "react-router-dom";
 
 export const HomePage = () => {
   const { data: user, isLoading } = useMe();
   const isAdmin  = useAuthStore((s) => s.isAdmin);
-
-  if (isLoading) return <p>Loading…</p>;
+  if (isLoading) return <LoadingScreen message="Cargando HomePage, espere un momento por favor..."/>;
 
   return (
     <Box>
       <Heading size="lg">Bienvenido, {user?.name} {user?.lastName}</Heading>
       {/* <p>{user?.email} — {user?.roleName}</p> */}
       {isAdmin && <Link to="/register">Register new user</Link>}
+      <ErrorDialog trigger={<Button mt={4}>Mostrar error</Button>}  />
     </Box>
   );
-};
+};``
