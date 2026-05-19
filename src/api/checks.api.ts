abbf141710be414418c8bb6d9f7ea3b67f4330c8
Math2okt <matias.marecos@fiuni.edu.py>
2026-05-19 15:46:46 -0300

    feat: check api connection

--- src/api/checks.api.ts
+++ src/api/checks.api.ts
@@ -1,6 +1,15 @@
 import type { PaginationParams, PaginationType } from "@/types/types";
 import { apiClient } from "./client";
 
+export const checkStatusEnum:Record<number, string>  = { 
+    0: "Pendiente",
+    1: "Cobrado",
+    2: "Anulado"}
+
+export const checkTypeEnum:Record<number, string>  = { 
+    0: "Día",
+    1: "Diferido"
+}
 export interface GetCheckResponse {
     checks: Check[];
     pagination: PaginationType;
@@ -35,6 +44,6 @@ export interface UpdateCheckBodyRequest {
 export const checksApi = {
     getChecks:  (params?:PaginationParams) => apiClient.get<GetCheckResponse>("/api/Checks/", { params }).then((r) => r.data),
     createCheck: (body: CreateCheckBodyRequest) => apiClient.post<Check>("/api/Checks/",body).then((r) => r.data),
-    getCheckById: (id: number) => apiClient.get<Check>(`/api/Checks/${id}`).then((r) => r.data),
+    getCheckById: (id: number) => apiClient.get<{check: Check}>(`/api/Checks/${id}`).then((r) => r.data.check),
     updateCheck: (id: number, body: UpdateCheckBodyRequest) => apiClient.patch<Check>(`/api/Checks/${id}/status`, body).then((r) => r.data)
 }
